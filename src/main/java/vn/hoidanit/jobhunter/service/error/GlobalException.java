938e8757fbcb685154dd4e66d307f87e7a0aaeee
Tien Thanh <nguyenthanh2542001@gmail.com>
2024-11-24 20:50:52 +0700

    Handle Exception: Format Response bofore Sending

--- src/main/java/vn/hoidanit/jobhunter/service/error/GlobalException.java
+++ src/main/java/vn/hoidanit/jobhunter/service/error/GlobalException.java
@@ -1,15 +1,23 @@
 package vn.hoidanit.jobhunter.service.error;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import vn.hoidanit.jobhunter.domain.RestResponse;
 
 @RestControllerAdvice
 public class GlobalException {
 
     // https://docs.spring.io/spring-framework/docs/current/javadoc-api/org/springframework/web/bind/annotation/ControllerAdvice.html
     @ExceptionHandler(value = IdInvalidException.class)
-    public ResponseEntity<String> handleIdAlreadyExistsException(IdInvalidException idException) {
-        return ResponseEntity.badRequest().body(idException.getMessage());
+    public ResponseEntity<RestResponse<Object>> handleIdAlreadyExistsException(IdInvalidException idException) {
+        RestResponse<Object> res = new RestResponse<Object>();
+        res.setStatusCode(HttpStatus.BAD_REQUEST.value());
+        res.setError(idException.getMessage());
+        res.setMessage("IdInvalidException");
+        
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(res);
     }
 }
