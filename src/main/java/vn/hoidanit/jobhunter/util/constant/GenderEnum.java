10c050fa847107dfa94cdc08a92010c1b49a16fc
Tien Thanh <nguyenthanh2542001@gmail.com>
2024-12-09 22:39:21 +0700

    Modules User: Update Entity User and Enum in java

---
+++ src/main/java/vn/hoidanit/jobhunter/util/constant/GenderEnum.java
@@ -0,0 +1,9 @@
+package vn.hoidanit.jobhunter.util.constant;
+
+public enum GenderEnum {
+    // Khai báo constant
+    FEMALE, MALE, OTHER;
+
+
+    // Enum: https://dev.to/noelopez/spring-rest-working-with-enums-ma
+}
