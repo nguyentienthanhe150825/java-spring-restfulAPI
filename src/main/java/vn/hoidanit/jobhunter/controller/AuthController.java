aee03d03dde2575e2822fc9d1d437f6edcce3bf7
Tien Thanh <nguyenthanh2542001@gmail.com>
2024-12-10 14:52:16 +0700

    Modules User: API Login and Handle Data Response include acces_token and data user

--- src/main/java/vn/hoidanit/jobhunter/controller/AuthController.java
+++ src/main/java/vn/hoidanit/jobhunter/controller/AuthController.java
@@ -11,8 +11,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import jakarta.validation.Valid;
+import vn.hoidanit.jobhunter.domain.User;
 import vn.hoidanit.jobhunter.domain.dto.LoginDTO;
 import vn.hoidanit.jobhunter.domain.dto.ResLoginDTO;
+import vn.hoidanit.jobhunter.domain.dto.ResLoginDTO.UserLogin;
+import vn.hoidanit.jobhunter.service.UserService;
 import vn.hoidanit.jobhunter.util.SecurityUtil;
 
 @RestController
@@ -23,10 +26,13 @@ public class AuthController {
     // https://github.com/hoidanit-be-java-spring-rest/02-java-jhipster-with-filter/blob/master/src/main/java/com/mycompany/myapp/web/rest/AuthenticateController.java?ref_type=heads#L54
     private final AuthenticationManagerBuilder authenticationManagerBuilder;
     private final SecurityUtil securityUtil;
+    private final UserService userService;
 
-    public AuthController(AuthenticationManagerBuilder authenticationManagerBuilder, SecurityUtil securityUtil) {
+    public AuthController(AuthenticationManagerBuilder authenticationManagerBuilder, SecurityUtil securityUtil,
+            UserService userService) {
         this.authenticationManagerBuilder = authenticationManagerBuilder;
         this.securityUtil = securityUtil;
+        this.userService = userService;
     }
 
     @PostMapping("/login")
@@ -46,6 +52,20 @@ public class AuthController {
 
         // Format response access_token
         ResLoginDTO res = new ResLoginDTO();
+
+        // Query database để lấy thông tin user sau đó gán vào UserLogin
+        User currentUserDB = this.userService.handleGetUserByUsername(loginDTO.getUsername());
+        if (currentUserDB != null) {
+            // Set Data into Inner class: UserLogin
+            UserLogin resUserLogin = res.new UserLogin();
+            
+            resUserLogin.setId(currentUserDB.getId());
+            resUserLogin.setEmail(currentUserDB.getEmail());
+            resUserLogin.setName(currentUserDB.getName());
+
+            res.setUser(resUserLogin);
+        }
+
         res.setAccessToken(access_token);
 
         return ResponseEntity.ok().body(res);
