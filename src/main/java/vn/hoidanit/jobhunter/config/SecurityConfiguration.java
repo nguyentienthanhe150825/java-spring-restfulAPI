da481abf96230b0176ca19b8f81e22f059a47944
Tien Thanh <nguyenthanh2542001@gmail.com>
2024-12-17 21:56:21 +0700

    Modules Job/Resume: Read File From Static Path

--- src/main/java/vn/hoidanit/jobhunter/config/SecurityConfiguration.java
+++ src/main/java/vn/hoidanit/jobhunter/config/SecurityConfiguration.java
@@ -47,7 +47,9 @@ public class SecurityConfiguration {
                 .csrf(c -> c.disable())
                 .cors(Customizer.withDefaults())
                 .authorizeHttpRequests(authorize -> authorize
-                        .requestMatchers("/", "/api/v1/auth/login", "/api/v1/auth/refresh").permitAll()
+                        .requestMatchers("/", "/api/v1/auth/login",
+                                "/api/v1/auth/refresh", "/storage/**")
+                        .permitAll()
                         .anyRequest().authenticated())
                 .oauth2ResourceServer((oauth2) -> oauth2.jwt(Customizer.withDefaults())
                         .authenticationEntryPoint(customAuthenticationEntryPoint))
