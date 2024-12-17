f189a6328c1d428c1ac813b26a38c8e9644e0f48
Tien Thanh <nguyenthanh2542001@gmail.com>
2024-12-17 23:00:01 +0700

    Modules Upload File: Create Folder

--- src/main/java/vn/hoidanit/jobhunter/config/StaticResourcesWebConfiguration.java
+++ src/main/java/vn/hoidanit/jobhunter/config/StaticResourcesWebConfiguration.java
@@ -8,12 +8,12 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class StaticResourcesWebConfiguration implements WebMvcConfigurer {
 
-    @Value("${hoidanit.upload-file.base-path}")
-    private String basePath;
+    @Value("${hoidanit.upload-file.base-uri}")
+    private String baseURI;
 
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("/storage/**").addResourceLocations(basePath);
+        registry.addResourceHandler("/storage/**").addResourceLocations(baseURI);
     }
 
     // #99 Config Read File From Path
---
+++ src/main/java/vn/hoidanit/jobhunter/service/FileService.java
@@ -0,0 +1,34 @@
+package vn.hoidanit.jobhunter.service;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class FileService {
+
+    public void createDirectory(String folder) throws URISyntaxException {
+        URI uri = new URI(folder);
+        Path path = Paths.get(uri);
+        File tmpDir = new File(path.toString());
+        if (!tmpDir.isDirectory()) {
+            try {
+                Files.createDirectory(tmpDir.toPath());
+                System.out.println(">>> CREATE NEW DIRECTORY SUCCESSFUL, PATH = " + folder);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        } else {
+            System.out.println(">>> SKIP MAKING DIRECTORY, ALREADY EXISTS");
+        }
+
+    }
+
+    // https://stackoverflow.com/questions/53251949/how-to-create-a-permanent-directory-for-my-files-with-spring-boot/53252034#53252034
+}
