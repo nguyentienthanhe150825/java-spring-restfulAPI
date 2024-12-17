ea158f15d17960acd6ac9eb38dc14774f4f33b9f
Tien Thanh <nguyenthanh2542001@gmail.com>
2024-12-18 01:15:14 +0700

    Modules Job/Resume: Refactoring Job Module ResCreateJobDTO

--- src/main/java/vn/hoidanit/jobhunter/controller/JobController.java
+++ src/main/java/vn/hoidanit/jobhunter/controller/JobController.java
@@ -19,8 +19,8 @@ import com.turkraft.springfilter.boot.Filter;
 
 import jakarta.validation.Valid;
 import vn.hoidanit.jobhunter.domain.Job;
-import vn.hoidanit.jobhunter.domain.response.ResCreateJobDTO;
 import vn.hoidanit.jobhunter.domain.response.ResultPaginationDTO;
+import vn.hoidanit.jobhunter.domain.response.job.ResCreateJobDTO;
 import vn.hoidanit.jobhunter.domain.response.job.ResUpdateJobDTO;
 import vn.hoidanit.jobhunter.service.JobService;
 import vn.hoidanit.jobhunter.util.annotation.ApiMessage;
