b06a922b9ce5db4e6c29de09fb1044b7fdb25d48
Tien Thanh <nguyenthanh2542001@gmail.com>
2024-12-18 15:12:49 +0700

    Modules Download File: Download single file use InputStreamResource

--- src/main/java/vn/hoidanit/jobhunter/controller/FileController.java
+++ src/main/java/vn/hoidanit/jobhunter/controller/FileController.java
@@ -1,5 +1,6 @@
 package vn.hoidanit.jobhunter.controller;
 
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.net.URISyntaxException;
 import java.time.Instant;
@@ -7,7 +8,12 @@ import java.util.Arrays;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.InputStreamResource;
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -62,5 +68,32 @@ public class FileController {
         res.setUploadedAt(Instant.now());
 
         return ResponseEntity.ok().body(res);
+    }
+
+    @GetMapping("/files")
+    @ApiMessage("Download a file")
+    public ResponseEntity<Resource> downloadFile(
+            @RequestParam(name = "fileName", required = false) String fileName, 
+            @RequestParam(name = "folder", required = false) String folder) throws StorageException, URISyntaxException, FileNotFoundException {
+        
+        // check file or folder exist
+        if (fileName == null || folder == null) {
+            throw new StorageException("Missing request param: (fileName or folder not exist)");
+        } 
+
+        // check file exist (and not a directory)
+        long fileLength = this.fileService.getFileLength(fileName, baseURI + folder);
+        if (fileLength == 0) {
+            throw new StorageException("File with name = " + fileName + " not found");
+        }
+
+        // download single file
+        InputStreamResource resource = this.fileService.getResource(fileName, baseURI + folder);
+
+        return ResponseEntity.ok()
+                .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + fileName + "\"")
+                .contentLength(fileLength)
+                .contentType(MediaType.APPLICATION_OCTET_STREAM)
+                .body(resource);
     }
 }
--- src/main/java/vn/hoidanit/jobhunter/service/FileService.java
+++ src/main/java/vn/hoidanit/jobhunter/service/FileService.java
@@ -1,6 +1,8 @@
 package vn.hoidanit.jobhunter.service;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.URI;
@@ -10,6 +12,7 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.StandardCopyOption;
 
+import org.springframework.core.io.InputStreamResource;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -45,6 +48,28 @@ public class FileService {
         return finalName;
     }
 
+    public long getFileLength(String fileName, String folder) throws URISyntaxException {
+        URI uri = new URI(folder + "/" + fileName);
+        Path path = Paths.get(uri);
+
+        File tmpDir = new File(path.toString());
+
+        // file not exist or file is 1 directory (thư mục thì sẽ ko có kích thước tệp)
+        if (!tmpDir.exists() || tmpDir.isDirectory()) {
+            return 0;
+        }
+        return tmpDir.length();
+    }
+
+    public InputStreamResource getResource(String fileName, String folder) throws URISyntaxException, FileNotFoundException {
+        URI uri = new URI(folder + "/" + fileName);
+        Path path = Paths.get(uri);
+
+        File file = new File(path.toString());
+        return new InputStreamResource(new FileInputStream(file));
+    }
+
     // store file: https://spring.io/guides/gs/uploading-files
     // https://stackoverflow.com/questions/53251949/how-to-create-a-permanent-directory-for-my-files-with-spring-boot/53252034#53252034
+    // download file: https://stackoverflow.com/questions/35680932/download-a-file-from-spring-boot-rest-service
 }
