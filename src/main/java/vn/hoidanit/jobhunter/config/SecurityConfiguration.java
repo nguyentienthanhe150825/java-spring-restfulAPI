7cf0262e8e9b43b8b5264a2d87b40616be16b810
Tien Thanh <nguyenthanh2542001@gmail.com>
2024-12-18 21:13:43 +0700

    Config Endpoint Security

--- src/main/java/vn/hoidanit/jobhunter/config/SecurityConfiguration.java
+++ src/main/java/vn/hoidanit/jobhunter/config/SecurityConfiguration.java
@@ -43,12 +43,17 @@ public class SecurityConfiguration {
     public SecurityFilterChain filterChain(HttpSecurity http,
             CustomAuthenticationEntryPoint customAuthenticationEntryPoint) throws Exception {
         // v6 . lamda
+        String[] whiteList = {
+                "/",
+                "/api/v1/auth/login", "/api/v1/auth/refresh", "/storage/**",
+                "/api/v1/companies/**", "/api/v1/jobs/**"
+        };
+        
         http
                 .csrf(c -> c.disable())
                 .cors(Customizer.withDefaults())
                 .authorizeHttpRequests(authorize -> authorize
-                        .requestMatchers("/", "/api/v1/auth/login",
-                                "/api/v1/auth/refresh", "/storage/**")
+                        .requestMatchers(whiteList)
                         .permitAll()
                         .anyRequest().authenticated())
                 .oauth2ResourceServer((oauth2) -> oauth2.jwt(Customizer.withDefaults())
