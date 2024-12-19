08f16d1869577bb09ee6252c90b963610ecac6f8
Tien Thanh <nguyenthanh2542001@gmail.com>
2024-12-19 16:53:16 +0700

    Modules Permission/Role: API Update Permission

--- src/main/java/vn/hoidanit/jobhunter/controller/PermissionController.java
+++ src/main/java/vn/hoidanit/jobhunter/controller/PermissionController.java
@@ -3,6 +3,7 @@ package vn.hoidanit.jobhunter.controller;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -27,14 +28,34 @@ public class PermissionController {
     @ApiMessage("Create a permission") 
     public ResponseEntity<Permission> createNewPermission(@Valid @RequestBody Permission requestPermission) throws IdInvalidException {
         // check module, apiPath, method exist
-        boolean exist = this.permissionService.isPermissionExist(requestPermission);
-        if (exist) {
+        boolean isPermissionExist = this.permissionService.isPermissionExist(requestPermission);
+        if (isPermissionExist) {
             throw new IdInvalidException("Permission đã tồn tại");
         }
 
         Permission createPermission = this.permissionService.handleCreatePermission(requestPermission);
         return ResponseEntity.status(HttpStatus.CREATED).body(createPermission);
     } 
+
+    @PutMapping("/permissions")
+    @ApiMessage("Update a permission")
+    public ResponseEntity<Permission> updatePermission(@Valid @RequestBody Permission requestPermission) throws IdInvalidException {
+        // check id exist
+        Permission currentPermission = this.permissionService.fetchPermissionById(requestPermission.getId());
+        if (currentPermission == null) {
+            throw new IdInvalidException("Permission với id = " + requestPermission.getId() + " không tồn tại");
+        }
+
+        // check module, apiPath, method exist
+        boolean isPermissionExist = this.permissionService.isPermissionExist(requestPermission);
+        if (isPermissionExist) {
+            throw new IdInvalidException("Permission đã tồn tại");
+        }
+
+        Permission updatePermission = this.permissionService.handleUpdatePermission(requestPermission, currentPermission);
+
+        return ResponseEntity.status(HttpStatus.OK).body(updatePermission);
+    }
     
     
 }
--- src/main/java/vn/hoidanit/jobhunter/service/PermissionService.java
+++ src/main/java/vn/hoidanit/jobhunter/service/PermissionService.java
@@ -1,5 +1,7 @@
 package vn.hoidanit.jobhunter.service;
 
+import java.util.Optional;
+
 import org.springframework.stereotype.Service;
 
 import jakarta.validation.Valid;
@@ -16,10 +18,31 @@ public class PermissionService {
     }
 
     public boolean isPermissionExist(Permission permission) {
-        return this.permissionRepository.existsByModuleAndApiPathAndMethod(permission.getModule(), permission.getApiPath(), permission.getMethod());
+        return this.permissionRepository.existsByModuleAndApiPathAndMethod(permission.getModule(),
+                permission.getApiPath(), permission.getMethod());
     }
 
     public Permission handleCreatePermission(Permission permission) {
         return this.permissionRepository.save(permission);
+    }
+
+    public Permission fetchPermissionById(long id) {
+        Optional<Permission> permissionOptional = this.permissionRepository.findById(id);
+        if (permissionOptional.isPresent()) {
+            return permissionOptional.get();
+        }
+        return null;
+    }
+
+    public Permission handleUpdatePermission(Permission requestPermission, Permission currentPermission) {
+        // set name, apiPath, method, module
+        currentPermission.setName(requestPermission.getName());
+        currentPermission.setApiPath(requestPermission.getApiPath());
+        currentPermission.setMethod(requestPermission.getMethod());
+        currentPermission.setModule(requestPermission.getModule());    
+
+        // update
+        currentPermission = this.permissionRepository.save(currentPermission);
+        return currentPermission;
     }
 }
