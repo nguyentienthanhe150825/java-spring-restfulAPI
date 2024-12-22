74218227c3b9dda6bf093c0de2b8f6cc0416c086
Tien Thanh <nguyenthanh2542001@gmail.com>
2024-12-22 09:35:46 +0700

    Create Sample Data

--- src/main/java/vn/hoidanit/jobhunter/domain/Permission.java
+++ src/main/java/vn/hoidanit/jobhunter/domain/Permission.java
@@ -16,6 +16,7 @@ import jakarta.persistence.PreUpdate;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 import vn.hoidanit.jobhunter.util.SecurityUtil;
 
@@ -23,6 +24,7 @@ import vn.hoidanit.jobhunter.util.SecurityUtil;
 @Table(name = "Permissions")
 @Getter
 @Setter
+@NoArgsConstructor
 public class Permission {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -48,6 +50,13 @@ public class Permission {
     @ManyToMany(mappedBy = "permissions", fetch = FetchType.LAZY)
     @JsonIgnore
     private List<Role> roles;
+
+    public Permission(String name, String apiPath, String method, String module) {
+        this.name = name;
+        this.apiPath = apiPath;
+        this.method = method;
+        this.module = module;
+    }
 
     @PrePersist
     public void handleBeforeCreate() {
