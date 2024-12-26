4b57ab38581498d7b4f423eebe19295e0737dce0
Tien Thanh <nguyenthanh2542001@gmail.com>
2024-12-26 20:45:46 +0700

    Modules Subscribers: API Update Subscriber

--- src/main/java/vn/hoidanit/jobhunter/controller/SubscriberController.java
+++ src/main/java/vn/hoidanit/jobhunter/controller/SubscriberController.java
@@ -3,6 +3,7 @@ package vn.hoidanit.jobhunter.controller;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -33,6 +34,19 @@ public class SubscriberController {
         }
         Subscriber newSubscriber = this.subscriberService.handleCreateSubscriber(requestSubscriber);
         return ResponseEntity.status(HttpStatus.CREATED).body(newSubscriber);
+    }
+
+
+    @PutMapping("/subscribers")
+    @ApiMessage("Update a subscriber")
+    public ResponseEntity<Subscriber> updateSubscriber (@RequestBody Subscriber requestSubscriber) throws IdInvalidException {
+        // check subscriber by id
+        Subscriber currentSubscriber = this.subscriberService.fetchSubscriberById(requestSubscriber.getId());
+        if (currentSubscriber == null) {
+            throw new IdInvalidException("Id " + requestSubscriber.getId() + " không tồn tại");
+        }
+        Subscriber updateSubscriber = this.subscriberService.updateSubscriber(currentSubscriber, requestSubscriber);
+        return ResponseEntity.status(HttpStatus.OK).body(updateSubscriber);
     }
 
 }
--- src/main/java/vn/hoidanit/jobhunter/service/SubscriberService.java
+++ src/main/java/vn/hoidanit/jobhunter/service/SubscriberService.java
@@ -3,10 +3,10 @@ package vn.hoidanit.jobhunter.service;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 import org.springframework.stereotype.Service;
 
-import jakarta.validation.Valid;
 import vn.hoidanit.jobhunter.domain.Skill;
 import vn.hoidanit.jobhunter.domain.Subscriber;
 import vn.hoidanit.jobhunter.repository.SkillRepository;
@@ -27,6 +27,14 @@ public class SubscriberService {
         return this.subscriberRepository.existsByEmail(email);
     }
 
+    public Subscriber fetchSubscriberById (long id) {
+        Optional<Subscriber> subscriberOptional = this.subscriberRepository.findById(id);
+        if (subscriberOptional.isPresent()) {
+            return subscriberOptional.get();
+        }
+        return null;
+    }
+
     public Subscriber handleCreateSubscriber(Subscriber requestSubscriber) {
         // check Skill id exist
         if (requestSubscriber.getSkills() != null) {
@@ -36,10 +44,27 @@ public class SubscriberService {
             for (Skill skill : requestSubscriber.getSkills()) {
                 listIdSkill.add(skill.getId());
             }
-
+            // get List Skill by List Skill id
             List<Skill> listSkills = this.skillRepository.findByIdIn(listIdSkill);
             requestSubscriber.setSkills(listSkills);
         }
         return this.subscriberRepository.save(requestSubscriber);
+    }
+
+    public Subscriber updateSubscriber(Subscriber currentSubscriber, Subscriber requestSubscriber) {
+        // check skill id exist
+        if (requestSubscriber.getSkills() != null) {
+            // get List Skill id
+            List<Long> listIdSkill = requestSubscriber.getSkills()
+                    .stream().map(skill -> skill.getId())
+                    .collect(Collectors.toList());
+            
+            // get List Skill by List Skill id
+            List<Skill> listSkills = this.skillRepository.findByIdIn(listIdSkill);
+            // set currentSubscriber
+            currentSubscriber.setSkills(listSkills);
+        }
+        currentSubscriber = this.subscriberRepository.save(currentSubscriber);
+        return currentSubscriber;
     }
 }
