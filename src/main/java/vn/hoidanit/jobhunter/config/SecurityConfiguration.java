bc1763181aa7759710ba136202b8d681dc981298
Tien Thanh <nguyenthanh2542001@gmail.com>
2024-12-31 11:42:29 +0700

    Config Swagger and Config Bearer Token Swagger

--- src/main/java/vn/hoidanit/jobhunter/config/SecurityConfiguration.java
+++ src/main/java/vn/hoidanit/jobhunter/config/SecurityConfiguration.java
@@ -46,8 +46,12 @@ public class SecurityConfiguration {
         // v6 . lamda
         String[] whiteList = {
                 "/",
-                "/api/v1/auth/login", "/api/v1/auth/refresh", "/storage/**",
-                "/api/v1/companies/**", "/api/v1/jobs/**", "/api/v1/auth/register", "/api/v1/email/**"
+                "/api/v1/auth/login", "/api/v1/auth/refresh", "/api/v1/auth/register",
+                "/storage/**",
+                "/api/v1/email/**",
+                "/v3/api-docs/**",
+                "/swagger-ui/**",
+                "/swagger-ui.html"
         };
         
         http
